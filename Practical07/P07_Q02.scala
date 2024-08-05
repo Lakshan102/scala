1a335f495b1249d9f19dec1023011cbd439b6554
lakshan102 <lakshan2725@gmail.com>
2024-08-06 02:05:55 +0530

    practical 07 program code submission

---
+++ Practical07/P07_Q02.scala
@@ -0,0 +1,11 @@
+object CalculateSqureNo{
+    def calculateSquare(numbers: List[Int]): List[Int] = {
+    numbers.map(n => n * n)
+    }
+
+    def main(args: Array[String]): Unit = {
+        val input = List(1, 2, 3, 4, 5)
+        val output = calculateSquare(input)
+        println(output)  
+    }
+}
